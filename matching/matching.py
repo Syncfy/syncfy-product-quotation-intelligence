f42b2ac5c5c4153779fd51da6209e7e28af03cd9
Gabriel-Triceri <112666478+Gabriel-Triceri@users.noreply.github.com>
2024-03-06 14:02:46 -0300

    feat/fix: removing search.py, fixing match.py ​​and putting Generator.py into function

--- matching/matching.py
+++ matching/matching.py
@@ -1,36 +1,37 @@
+import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Lista de descrições dos produtos
-produtos = [
-    "Smartphone XYZ 64GB",
-    "Notebook ABC 15' Core i7",
-    "Smartwatch DEF Health Tracker",
-    # Adicione mais produtos conforme necessário
-]
-
-# Vetorização das descrições dos produtos
-vectorizer = TfidfVectorizer()
-produtos_tfidf = vectorizer.fit_transform(produtos)
+def carregar_descricoes_from_json(json_file_path):
+    with open(json_file_path, 'r') as file:
+        produtos = json.load(file)
+    descricoes = [produto['nome'] for produto in produtos]
+    return descricoes
 
 # Função para buscar produtos com base em uma query
-def buscar_produtos(query):
+def buscar_produtos_from_json(json_file_path, query):
+    descricoes = carregar_descricoes_from_json(json_file_path)
+
+    # Vetorização das descrições dos produtos
+    vectorizer = TfidfVectorizer()
+    produtos_tfidf = vectorizer.fit_transform(descricoes)
+
     # Vetorização da query
     query_tfidf = vectorizer.transform([query])
-    
+
     # Cálculo da similaridade do cosseno entre a query e os produtos
     similaridade = cosine_similarity(query_tfidf, produtos_tfidf).flatten()
-    
-    # Ordenação dos produtos por similaridade
-    indices_ordenados = similaridade.argsort()[::-1]
-    
-    resultados = [(produtos[i], similaridade[i]) for i in indices_ordenados]
+
+    # Filtragem dos produtos com score maior que 0
+    resultados = [(descricoes[i], similaridade[i]) for i in range(len(descricoes)) if similaridade[i] > 0]
     return resultados
 
-# Exemplo de busca
-query = "smartphone"
-resultados = buscar_produtos(query)
+if __name__ == "__main__":
+    # Exemplo de busca
+    json_file_path = 'produtos_simulados.json'
+    query = "smartphone"
+    resultados = buscar_produtos_from_json(json_file_path, query)
 
-# Exibindo os resultados
-for produto, score in resultados:
-    print(f"Produto: {produto}, Score: {score:.4f}")
+    # Exibindo os resultados
+    for produto, score in resultados:
+        print(f"Produto: {produto}, Score: {score:.4f}")
